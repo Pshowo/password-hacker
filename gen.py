75bf3e729983e02415d650446971245477391f05
Pawel <pjuszcze@gmail.com>
2021-02-21 00:47:34 +0100

    Changed: gens doc

--- gen.py
+++ gen.py
@@ -4,6 +4,12 @@ alp = ["a", "b", "c", "d", "e"]
 nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 def gens(num, alphabet, len, password):
+    """
+    Password generator
+    :params num: <list> with nums
+    :params alphabet: <list> with chars
+    :params len: <int> length password generator
+    """
     all_char = list(itertools.chain(num, alphabet))
     prods = itertools.product(all_char, repeat=len)
     a = ""
@@ -12,6 +18,6 @@ def gens(num, alphabet, len, password):
         a = "".join(a)
     print("STOP: Yours password is:", a) 
 
-
-
-gens(nums, alp, 4, "6b1d")
+if __name__ == "__main__":
+    print("RUNS")
+    gens(nums, alp, 4, "6b1d")
